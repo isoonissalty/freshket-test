ee7e5880e51c5eee009ddc639809ff6f48805456
Jin Walker <isoon.ph@gmail.com>
2020-07-10 14:50:52 +0700

    init first question

---
+++ src/components/Question1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Question1 = () => {
+  return (
+    <div>
+      <h2>Maximum 58 Number</h2>
+      <div>
+        <span>input: </span>
+        <input />
+      </div>
+      <div>
+        <span>output: </span>
+        <span>{answer}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Question1
