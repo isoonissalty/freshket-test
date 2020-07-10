f130ea3f2c6d91685ee4e3eb3a86d97c20003e76
Jin Walker <isoon.ph@gmail.com>
2020-07-10 16:09:42 +0700

    init Q2 components

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Question2 = () => {
+  const [input, setInput] = useState('')
+  const [answer, setAnswer] = useState('')
+
+  const handleOnchange = (e) => {
+    let { value } = e.target
+    setInput(value)
+  }
+
+  const longSubStr = (value) => {
+
+  }
+
   return (
     <div>
-      <h2></h2>
+      <h2>Longest Substring Without Repeating Characters</h2>
       <div>
         <span>input: </span>
+        <input value={input} onChange={handleOnchange} />
+        <button onClick={()=>longSubStr(input)}>submit</button>
+      </div>
+      <div>
+        <span>output: </span>
+        <span>{answer}</span>
       </div>
     </div>
   )
